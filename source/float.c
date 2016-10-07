3bbb81c5b2bdacd36122536941a567687a1ff6d2
zighouse <zighouse@hotmail.com>
2016-10-07 22:02:29 +0800

    improve algorithm of converting float to fraction.

--- source/float.c
+++ source/float.c
@@ -7,12 +7,9 @@
 #include "cf.h"
 
 static
-fraction double2fraction(double x, int * exp)
+fraction float_to_fraction(double x, int * exp)
 {
-    char *d, *p, *end;
-    const int size = sizeof(x);
-    char text[65] = {0};
-    int i, j;
+    char * data;
     fraction ret;
 
     switch (fpclassify(x))
@@ -33,34 +30,14 @@ fraction double2fraction(double x, int * exp)
     }
 
     x = frexp(x, exp);
-    d = (char*) &x;
+    data = (char*) &x;
 
-    // convert data of double to big-endian binary format
-    for (i = 0; i < size; ++i)
-    {
-        char buf[9] = {0};
-        for (j = 0; j < 8; ++j)
-        {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-            buf[7 - j] = (d[size - i - 1] & 1) + '0';
-            d[size - i - 1] >>= 1;
-#else
-            buf[7 - j] = (d[i] & 1) + '0';
-            d[i] >>= 1;
-#endif
-        }
-        strcat(text, buf);
-    }
-
-    // mantisa for double is from bit[12], and the first bit is always 1 and omited.
-    ret = (fraction) {x < 0.0 ? -1ll : 1ll, 2ll};
-    end = strrchr(text, '1');
-    for (p = &text[12]; p <= end; ++p)
-    {
-        ret.n <<= 1;
-        ret.d <<= 1;
-        ret.n |= (p[0] - '0');
-    }
+    /*
+     * the mantisa for double is from bit[12], and the first bit is always 1
+     * and is omited.
+     */
+    ret.n = (0x000fffffffffffffll & *(long long*)data) | 0x0010000000000000ll;
+    ret.d = 0x0020000000000000ll;
 
     return ret;
 }
@@ -70,7 +47,7 @@ cf * cf_create_from_float(double d)
     cf * ret;
     int exp;
 
-    fraction rat = double2fraction(d, &exp);
+    fraction rat = float_to_fraction(d, &exp);
     cf * x = cf_create_from_fraction(rat);
     if (exp > 0)
     {
@@ -78,7 +55,7 @@ cf * cf_create_from_float(double d)
     }
     else
     {
-        ret = cf_create_from_homographic(x, 1ll, 0ll, 0ll, 1ll<<-exp);
+        ret = cf_create_from_homographic(x, 1ll, 0ll, 0ll, 1ll << -exp);
     }
     cf_free(x);
     return ret;
